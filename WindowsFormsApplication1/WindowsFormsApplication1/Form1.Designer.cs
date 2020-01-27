8acd5bb72465d17aaf6907182aced6c3d4991e6d
Panda74 <60334241+Panda74@users.noreply.github.com>
2020-01-27 13:06:25 +0600

    Чат с локальным подключением
    Выполнен лично мною и Максом - норм чуваком

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tB_Username = new System.Windows.Forms.TextBox();
+            this.btn_LogIn = new System.Windows.Forms.Button();
+            this.btn_LogOut = new System.Windows.Forms.Button();
+            this.tB_Chat = new System.Windows.Forms.TextBox();
+            this.tB_Message = new System.Windows.Forms.TextBox();
+            this.btn_Enter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // tB_Username
+            // 
+            this.tB_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tB_Username.Location = new System.Drawing.Point(120, 17);
+            this.tB_Username.Name = "tB_Username";
+            this.tB_Username.Size = new System.Drawing.Size(299, 29);
+            this.tB_Username.TabIndex = 1;
+            // 
+            // btn_LogIn
+            // 
+            this.btn_LogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_LogIn.Location = new System.Drawing.Point(425, 14);
+            this.btn_LogIn.Name = "btn_LogIn";
+            this.btn_LogIn.Size = new System.Drawing.Size(118, 37);
+            this.btn_LogIn.TabIndex = 2;
+            this.btn_LogIn.Text = "Log In";
+            this.btn_LogIn.UseVisualStyleBackColor = true;
+            this.btn_LogIn.Click += new System.EventHandler(this.btn_LogIn_Click);
+            // 
+            // btn_LogOut
+            // 
+            this.btn_LogOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_LogOut.Location = new System.Drawing.Point(549, 14);
+            this.btn_LogOut.Name = "btn_LogOut";
+            this.btn_LogOut.Size = new System.Drawing.Size(118, 37);
+            this.btn_LogOut.TabIndex = 3;
+            this.btn_LogOut.Text = "Log Out";
+            this.btn_LogOut.UseVisualStyleBackColor = true;
+            this.btn_LogOut.Click += new System.EventHandler(this.btn_LogOut_Click);
+            // 
+            // tB_Chat
+            // 
+            this.tB_Chat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.tB_Chat.Location = new System.Drawing.Point(12, 65);
+            this.tB_Chat.Multiline = true;
+            this.tB_Chat.Name = "tB_Chat";
+            this.tB_Chat.Size = new System.Drawing.Size(655, 292);
+            this.tB_Chat.TabIndex = 4;
+            // 
+            // tB_Message
+            // 
+            this.tB_Message.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.tB_Message.Location = new System.Drawing.Point(12, 363);
+            this.tB_Message.Multiline = true;
+            this.tB_Message.Name = "tB_Message";
+            this.tB_Message.Size = new System.Drawing.Size(531, 81);
+            this.tB_Message.TabIndex = 5;
+            // 
+            // btn_Enter
+            // 
+            this.btn_Enter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Enter.Location = new System.Drawing.Point(549, 363);
+            this.btn_Enter.Name = "btn_Enter";
+            this.btn_Enter.Size = new System.Drawing.Size(118, 81);
+            this.btn_Enter.TabIndex = 6;
+            this.btn_Enter.Text = "Enter";
+            this.btn_Enter.UseVisualStyleBackColor = true;
+            this.btn_Enter.Click += new System.EventHandler(this.btn_Enter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(679, 456);
+            this.Controls.Add(this.btn_Enter);
+            this.Controls.Add(this.tB_Message);
+            this.Controls.Add(this.tB_Chat);
+            this.Controls.Add(this.btn_LogOut);
+            this.Controls.Add(this.btn_LogIn);
+            this.Controls.Add(this.tB_Username);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tB_Username;
+        private System.Windows.Forms.Button btn_LogIn;
+        private System.Windows.Forms.Button btn_LogOut;
+        private System.Windows.Forms.TextBox tB_Chat;
+        private System.Windows.Forms.TextBox tB_Message;
+        private System.Windows.Forms.Button btn_Enter;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        bool alive = false; // будет ли работать поток для приема
+        UdpClient client;
+        const int LOCALPORT = 8001; // порт для приема сообщений
+        const int REMOTEPORT = 8001; // порт для отправки сообщений
+        const int TTL = 20;
+        const string HOST = "235.5.5.1"; // хост для групповой рассылки
+        IPAddress groupAddress; // адрес для групповой рассылки
+
+        string userName; // имя пользователя в чате
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btn_LogIn.Enabled = true; // кнопка входа
+            btn_LogOut.Enabled = false; // кнопка выхода
+            btn_Enter.Enabled = false; // кнопка отправки
+            tB_Chat.ReadOnly = true; // поле для сообщений
+
+            groupAddress = IPAddress.Parse(HOST);
+        }
+
+        private void btn_LogIn_Click(object sender, EventArgs e)
+        {
+            userName = tB_Username.Text;
+            tB_Username.ReadOnly = true;
+
+            try
+            {
+                client = new UdpClient(LOCALPORT);
+                // присоединяемся к групповой рассылке
+                client.JoinMulticastGroup(groupAddress, TTL);
+
+                // запускаем задачу на прием сообщений
+                Task receiveTask = new Task(ReceiveMessages);
+                receiveTask.Start();
+
+                MySqlConnection conn = new MySqlConnection("server=localhost;user=root;password=;database=psevdochat");
+                conn.Open();
+                MySqlCommand cmd2 = new MySqlCommand("SELECT `DataTime`, `User`, `Text` FROM `messaging` WHERE 1", conn);
+                MySqlDataReader reader1 = cmd2.ExecuteReader();
+                while (reader1.Read())
+                {
+                    byte[] data = Encoding.Unicode.GetBytes(reader1[0].ToString());
+                    client.Send(data, data.Length, HOST, REMOTEPORT);
+                    //tB_Chat.Text = (reader1[0].ToString());
+                    reader1.Close();
+                }
+                conn.Close();
+                // отправляем первое сообщение о входе нового пользователя
+                string message = userName + " вошел в чат";
+                byte[] data1 = Encoding.Unicode.GetBytes(message);
+                client.Send(data1, data1.Length, HOST, REMOTEPORT);
+
+                btn_LogIn.Enabled = false;
+                btn_LogOut.Enabled = true;
+                btn_Enter.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // метод приема сообщений
+        private void ReceiveMessages()
+        {
+            alive = true;
+            try
+            {
+                while (alive)
+                {
+                    IPEndPoint remoteIp = null;
+                    byte[] data = client.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+
+                    // добавляем полученное сообщение в текстовое поле
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        string time = DateTime.Now.ToShortTimeString();
+                        tB_Chat.Text = time + " " + message + "\r\n" + tB_Chat.Text;
+                    }));
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                if (!alive)
+                    return;
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // обработчик нажатия кнопки sendButton
+        private void btn_Enter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = String.Format("{0}: {1}", userName, tB_Message.Text);
+                string time = String.Format("{0}:{1}");
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+                MySqlConnection conn = new MySqlConnection("server=localhost;user=root;password=;database=psevdochat");
+                conn.Open();
+                MySqlCommand cmd1 = new MySqlCommand("INSERT INTO `messaging`(`ID`, `DataTime`, `User`, `Text`) VALUES ('','"+time+"','" + userName + "','" + tB_Message.Text + "')", conn);
+                cmd1.ExecuteScalar();
+                conn.Close();
+                tB_Message.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // обработчик нажатия кнопки logoutButton
+        private void btn_LogOut_Click(object sender, EventArgs e)
+        {
+            ExitChat();
+        }
+        // выход из чата
+        private void ExitChat()
+        {
+            string message = userName + " покидает чат";
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            client.Send(data, data.Length, HOST, REMOTEPORT);
+            client.DropMulticastGroup(groupAddress);
+
+            alive = false;
+            client.Close();
+
+            btn_LogIn.Enabled = true;
+            btn_LogOut.Enabled = false;
+            btn_Enter.Enabled = false;
+        }
+        // обработчик события закрытия формы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (alive)
+                ExitChat();
+        }
+    }
+}
